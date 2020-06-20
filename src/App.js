11e3ae13173a9001f270ec8dfc83baabfa16e9a0
Damian Zmuda <d.zmuda94@gmail.com>
2020-06-20 20:47:41 +0200

    add basic folder structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import HomePage from 'pages/HomePage';
 
 function App() {
     return (
         <div className="App">
-            <h1>React app</h1>
+            <HomePage />
         </div>
     );
 }
